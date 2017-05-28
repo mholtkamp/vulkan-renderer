31b439f52dc6fc52343acd4ee3a29219043915b3
Martin Holtkamp <mholt012@gmail.com>
2017-05-28 18:03:52 -0400

    Pauses compile.bat if a shader failed to compile.

--- VulkanRenderer/Shaders/compile.bat
+++ VulkanRenderer/Shaders/compile.bat
@@ -1,10 +1,7 @@
-#%VULKAN_SDK%/Bin32/glslangValidator.exe -V shader.vert
-#%VULKAN_SDK%/Bin32/glslangValidator.exe -V shader.frag
-#pause
-
 for %%f in (.\src\*) do (
 
-  %VULKAN_SDK%/Bin32/glslangValidator.exe -V %%f -o .\bin\%%~nxf
+  (%VULKAN_SDK%/Bin32/glslangValidator.exe -V %%f -o .\bin\%%~nxf) && (
+     echo Compile Successful
+  ) || (
+  pause )
 )
-
-pause
